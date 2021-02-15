efe50698977c65b9b474df04918a8942055ec931
madelinemc <madeline.coons@gmail.com>
2021-02-15 13:41:25 -0600

    commit tree 91a8b96d4fcae7649ca164134393c88c9a76f7a7

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+  const directorList = directors.map(director => {
+    return(
+      <div>
+        <h3>Name: {director.name}</h3>
+        <ul>
+          {director.movies.map(movie => <li>{movie}</li>)}
+        </ul>
+      </div>
+    )
+  })
+  
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Pages</h1>
+      {directorList}
     </div>
   );
 }
